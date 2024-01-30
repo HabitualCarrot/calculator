156edd3c877da802b05ea6ba071cd866b631fb05
Will <156862405+HabitualCarrot@users.noreply.github.com>
2024-01-30 16:03:53 -0500

    Add basic mathematic functions

---
+++ script.js
@@ -0,0 +1,28 @@
+const numA
+const numB
+const opp
+
+
+
+// Additon function
+
+const addition = function(a, b) {
+    return (a + b)
+}
+
+// Subtraction function
+
+const subtraction = function(a, b) {
+    return (a - b)
+}
+
+// multiplication function
+
+const multiply = function(a, b) {
+    return (a * b) 
+}
+
+// division function
+const divide = function(a, b) {
+    return (a / b)
+}
